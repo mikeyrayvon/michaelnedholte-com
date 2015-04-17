ac8356e86ffedb82788befc9f862ba520b3a8d5d
mikey <michaelrayvon@gmail.com>
2015-04-17 14:14:22 -0500

    cleans up schema

--- footer.php
+++ footer.php
@@ -8,26 +8,8 @@
   <script type="application/ld+json">
     {
       "@context": "http://schema.org",
-      "@type": "Organization",
-      "url": "http://www.example.com",
-      "logo": "http://www.example.com/images/logo.png",
-      "contactPoint" : [
-        { "@type" : "ContactPoint",
-          "telephone" : "+1-877-746-0909",
-          "contactType" : "customer service",
-          "contactOption" : "TollFree",
-          "areaServed" : "US"
-        } , {
-          "@type" : "ContactPoint",
-          "telephone" : "+1-505-998-3793",
-          "contactType" : "customer service"
-        } ],
-      "sameAs" : [
-        "http://www.facebook.com/your-profile",
-        "http://instagram.com/yourProfile",
-        "http://www.linkedin.com/in/yourprofile",
-        "http://plus.google.com/your_profile"
-        ]
+      "@type": "Person",
+      "url": "http://michaelnedholte.com/",
     }
   </script>
 
