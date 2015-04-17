3d5c8c3d0dd01df3b7868ab16238887d3832fd6a
mikey <michaelrayvon@gmail.com>
2015-04-17 01:32:56 -0500

    wraps title in h1

--- page.php
+++ page.php
@@ -17,7 +17,7 @@ if( have_posts() ) {
 
     <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
       <a href="<?php the_permalink() ?>">
-        <h2><?php the_title(); ?></h2>
+        <h1><?php the_title(); ?></h1>
       </a>
 
       <?php the_content(); ?>
