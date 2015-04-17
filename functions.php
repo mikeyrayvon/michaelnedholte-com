a73502c2077ebd1c446821db7e2cd0ff03cc04a1
mikey <michaelrayvon@gmail.com>
2015-04-17 14:14:02 -0500

    adds blogName localized script

--- functions.php
+++ functions.php
@@ -8,6 +8,9 @@ function scripts_and_styles_method() {
   wp_enqueue_script( 'jslib', $jslib,'','',true);
   $myscripts = $templateuri."main.js";
   wp_enqueue_script( 'myscripts', $myscripts,'','',true);
+  wp_localize_script( 'myscripts', 'WP', array(
+    'blogName' => get_bloginfo('name')
+  ));
 
   // enqueue stylesheet here. file does not exist until stylus file is processed
   wp_enqueue_style( 'site', get_stylesheet_directory_uri() . '/css/site.css' );
