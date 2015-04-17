61c8118510de6c7d8e237a5d7d54e2cdfe3a4df0
mikey <michaelrayvon@gmail.com>
2015-04-17 14:16:52 -0500

    fixes breadcrumbs.  adds mobile support.  fixes nav.  fixes history

--- js/main.js
+++ js/main.js
@@ -9,36 +9,69 @@ function l(data) {
 var 
 	$header = $('#header'),
 	$mainContent = $('#main-content'),
+	openToggle = '&#9776;',
+	closeToggle = '&#x2715;',
 	parentId,
 	href,
 	posts,
-	crumb,
+	crumbs,
+	newHref,
+	winInitWidth = $(window).width(),
 	animateSpeed = 400,
 	headHeight = $('#header h1').outerHeight(),
 	margin = 20,
 	initHeight = headHeight+margin;
 
+if (typeof parents !== 'undefined') {
+	$('#breadcrumbs').html(parents);
+}
+
+
 
 // HEADER
-var animateAutoHeight = function() {
+var openNav = function() {
+	$('#breadcrumbs').addClass('hide');
 	$header.animateAuto(animateSpeed, function() {
 		//set new auto height to static height
 		headHeight = $header.outerHeight();
-		$header.css('height',headHeight);
+		$header.css('height',headHeight).addClass('js-nav-open');
+		$('#toggle').html(closeToggle);
 	});
 };
 
-$header.css('height', initHeight+'px')
-.hover(
+var closeNav = function() {
+	$('#breadcrumbs').removeClass('hide');
+	$header.stop().animate({height: initHeight}, animateSpeed, function() {
+		$('.crumb').removeClass('nav-selected');
+		$('.current').addClass('nav-selected');
+		$('.second-level.menu, .third-level.menu').addClass('nav-hide');
+		$('.current').closest('ul').removeClass('nav-hide');
+		$header.removeClass('js-nav-open');
+		$('#toggle').html(openToggle);
+	});
+};
+
+$header.css('height', initHeight+'px');
+
+$header.hover(
 	function() {
-		animateAutoHeight();
+		openNav();
 	},
 	function() {
-		$header.stop().animate({height: initHeight}, animateSpeed);
+		closeNav();
 	}
 );
 
+$('#toggle').on('click', function() {
+	if ($header.hasClass('js-nav-open')) {
+		closeNav();
+	} else {
+		openNav();
+	}
+});
+
 $mainContent.css('margin-top', initHeight);
+
 
 
 // NAV
@@ -46,17 +79,12 @@ $('.first-level.parent').on('click', function() {
 	$('.parent').removeClass('nav-selected');
 	$('.second-level.menu, .third-level.menu').addClass('nav-hide');
 
-	$('#breadcrumbs').addClass('hide');
-
 	$(this).addClass('nav-selected');
 
-	crumb = $(this).text();
-	$('#breadcrumbs').html('<span class="first-crumb crumb">'+crumb+'</span>');
-	
 	parentId = $(this).attr('data-id');
 	$('[data-parent="'+parentId+'"]').removeClass('nav-hide');
 
-	animateAutoHeight();
+	openNav();
 });
 
 $('.second-level.parent').on('click', function() {
@@ -65,68 +93,83 @@ $('.second-level.parent').on('click', function() {
 
 	$(this).addClass('nav-selected');
 
-	$('#breadcrumbs').addClass('hide');
-
-	$('.second-crumb, .third-crumb').remove();
-	crumb = $(this).text();
-	$('#breadcrumbs').append('<span class="second-crumb crumb">&rarr;'+crumb+'</span>');
-	
 	parentId = $(this).attr('data-id');
 	$('[data-parent="'+parentId+'"]').removeClass('nav-hide');
 
-	animateAutoHeight();
-});
-
-$('.third-level .menu-item .crumb').on('click', function() {
-	$('.third-crumb').remove();
-	crumb = $(this).text();
-	$('#breadcrumbs').append('<span class="third-crumb crumb">&rarr;'+crumb+'</span>');
+	openNav();
 });
 
 
+// ROUTER
+var Router = {
+  init: function() {
+    var _this = this;
+    window.onstatechange = function() {
+      newHref = window.location.href;
 
-//AJAX
-$('body').on('click', '.js-ajax-item', function(e) {
-	href = $(this).attr('href');
+			$('#main-container').addClass('loading');
 
-	if ($(this).hasClass('js-menu-item')) {
-		$('.js-ajax-item').removeClass('nav-selected');
-		$(this).addClass('nav-selected');
-		$header.stop().animate({height: initHeight}, animateSpeed);
-		$('#breadcrumbs').removeClass('hide');
-	}
+			$mainContent.animate({'opacity':0}, animateSpeed, function(data) {
+				$('html, body').animate({ scrollTop: '0px' }, animateSpeed/2);
 
-	if ($(this).hasClass('all')) {
-		$('.second-level.parent').removeClass('nav-selected');
-		$('.third-level.menu').addClass('nav-hide');
-		$(this).addClass('nav-selected');
-		$('.second-crumb, .third-crumb').remove();
-		crumb = $(this).text();
-		$('#breadcrumbs').append('<span class="second-crumb crumb">&rarr;'+crumb+'</span>');
-	}
-	
-	$('#main-container').addClass('loading');
- 
-	history.pushState(null,blogName,href);
-
-	$mainContent.animate({'opacity':0}, animateSpeed, function(data) {
-		$('html, body').animate({ scrollTop: '0px' }, animateSpeed/2);
-
-		$.ajax({
-			url: href,
-			success: function(data) {
-				posts = $(data).find('#posts');
-				console.log(posts);
-				$mainContent.html(posts);
-			}
-		}).done(function() {
-			$mainContent.animate({'opacity':1}, animateSpeed, function() {
-				$('#main-container').removeClass('loading');
+				$.ajax({
+					url: newHref,
+					success: function(data) {
+						posts = $(data).find('#posts');
+						$mainContent.html(posts);
+					}
+				}).done(function() {
+					$mainContent.animate({'opacity':1}, animateSpeed, function() {
+						$('#main-container').removeClass('loading');
+					});
+				});
 			});
-		});
+    };
+  },
+  loadHref: function(href) {
+    History.pushState(null, WP.blogName, href);
+  },
+};
+
+
+
+//TRIGGER
+if (jQuery.browser.mobile === false) {
+	$('body').on('click', '.js-ajax-item', function(e, data) {
+		href = $(this).attr('href');
+
+		if ($(this).hasClass('all')) {
+			$('.second-level.parent').removeClass('nav-selected');
+			$('.third-level.menu').addClass('nav-hide');
+			$(this).addClass('nav-selected');
+		}
+
+		if ($(this).hasClass('js-menu-item')) {
+			$('.js-ajax-item').removeClass('nav-selected');
+			$(this).addClass('nav-selected');
+			$header.stop().animate({height: initHeight}, animateSpeed);
+			$('.crumb').removeClass('current');
+			$('.nav-selected').addClass('current');
+
+			crumbs = $('.first-level.current').text() + '&rarr;' + $('.second-level.current').text();
+			if ($('.third-level.current').length) {
+				crumbs += '&rarr;' + $('.third-level.current').text();
+			}
+			$('#breadcrumbs').html(crumbs);
+			crumbs = '';
+		}
+	 
+		Router.loadHref(href);
+
+		return false;
 	});
-	return false;
-});
+
+	//ROUTER INIT
+	Router.init();
+} else {
+	$('#toggle').css('display','block');
+}
+
 
 
 jQuery(document).ready(function () {
