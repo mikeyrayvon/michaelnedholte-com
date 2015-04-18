761251a501cfa9399c1d3bb7361fc4c064cc0ef0
mikey <michaelrayvon@gmail.com>
2015-04-18 11:45:25 -0500

    remove commas from dates

--- archive.php
+++ archive.php
@@ -30,7 +30,7 @@ if( have_posts() ) {
 
     <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
       <h2><a href="<?php the_permalink() ?>" class="js-ajax-item news-item"><?php the_title(); ?></a></h2>
-      <span class="date"><?php echo get_the_date('d F, Y'); ?></span>
+      <span class="date"><?php echo get_the_date('d F Y'); ?></span>
       <?php the_content(); ?>
     </article>
 
@@ -39,7 +39,7 @@ if( have_posts() ) {
     <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
       <a href="<?php the_permalink() ?>" class="js-ajax-item">
         <h2><?php the_title(); ?></h2>
-        <span><?php echo get_the_date('F, Y'); ?></span>
+        <span><?php echo get_the_date('F Y'); ?></span>
       </a>
     </article>
 
